dcb072d8f122a8d2c5afe1043c0d6e661b42c147
rebeccaquintino <rebeccaqquintino@gmail.com>
2025-02-24 16:42:10 -0300

    hdl:memTest:memTestDevice:verilog corrected the comparator source file, the flow of output o_zero was wrong

--- hdl/memTest/memTestDevice/verilog/comparator.sv
+++ hdl/memTest/memTestDevice/verilog/comparator.sv
@@ -6,27 +6,33 @@
  */
 
 module comparator #(
-    parameter int p_WIDTH = 8 // Width of input data
+    parameter p_WIDTH = 8  // Defines the bit width of the input vectors
 )(
-    input  logic [p_WIDTH-1:0] i_DIN0, // First input data
-    input  logic [p_WIDTH-1:0] i_DIN1, // Second input data
-    output logic o_ZERO,  // Output signal indicating if any input is zero
-    output logic o_EQUAL, // Output signal indicating if i_DIN0 == i_DIN1
-    output logic o_LESS   // Output signal indicating if i_DIN0 < i_DIN1
+    input  logic [p_WIDTH-1:0] i_DIN0,  // First input vector
+    input  logic [p_WIDTH-1:0] i_DIN1,  // Second input vector
+    output logic o_ZERO,                // Output signal: '1' if any input is zero, else '0'
+    output logic o_EQUAL,               // Output signal: '1' if i_DIN0 == i_DIN1, else '0'
+    output logic o_LESS                 // Output signal: '1' if i_DIN0 < i_DIN1, else '0'
 );
 
-    // Check if any input is zero
+    // Process to check if i_DIN0 is zero
     always_comb begin
-        o_ZERO = (|i_DIN0) ? 1'b0 : 1'b1;
+        // If all bits of i_DIN0 are '0', set o_ZERO to '1'.
+        // Otherwise, set o_ZERO to '0'.
+        o_ZERO = (i_DIN0 == {p_WIDTH{1'b0}}) ? 1'b1 : 1'b0;
     end
 
-    // Check if the inputs are equal
+    // Process to check if both input vectors are equal
     always_comb begin
+        // If i_DIN0 is equal to i_DIN1, set o_EQUAL to '1'.
+        // Otherwise, set o_EQUAL to '0'.
         o_EQUAL = (i_DIN0 == i_DIN1) ? 1'b1 : 1'b0;
     end
 
-    // Check if i_DIN0 is less than i_DIN1
+    // Process to check if i_DIN0 is less than i_DIN1
     always_comb begin
+        // If i_DIN0 is less than i_DIN1, set o_LESS to '1'.
+        // Otherwise, set o_LESS to '0'.
         o_LESS = (i_DIN0 < i_DIN1) ? 1'b1 : 1'b0;
     end
 
